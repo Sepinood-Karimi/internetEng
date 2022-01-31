f36fc0fffd3c31af370afc95379dc4880b87a0e9
Sepinood Karimi <SepinoodKarimi@gmail.com>
2022-01-31 17:05:50 +0330

    fix admin problem

--- patients/urls.py
+++ patients/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 
-urlpatterns = {
+urlpatterns = [
     path('', views.index),
     path('patients/', views.patients),
     path('patients/<int:patient_id>/', views.patient)
-}
+]
